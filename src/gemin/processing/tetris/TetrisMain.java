069f82fd281f09c3c5324367771dcc587f01d22d
Jaemin Kim <geminik23@gmail.com>
2017-02-07 22:55:44 +0900

    init commit

---
+++ src/gemin/processing/tetris/TetrisMain.java
@@ -0,0 +1,18 @@
+package gemin.processing.tetris;
+
+import processing.core.PApplet;
+
+public class TetrisMain extends PApplet{
+
+	
+	public void settings(){
+		size(800,800);
+	}
+	
+	
+	/** Main */
+	public static void main(String[] args) {
+		PApplet.main("gemin.processing.tetris.TetrisMain");
+	}
+
+}
